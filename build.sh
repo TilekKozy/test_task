e0057654a0aed9f48a6d9e2a06cf96344890643b
tilek <tilek970122@gmail.com>
2023-09-04 09:33:29 +0600

    bash script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 docker run --rm -v $(pwd):/app prooph/composer:8.2 install
 cp .env.example .env
 ./vendor/bin/sail up -d
-./vendor/bin/sail npm install & npm run build
+./vendor/bin/sail npm install
+./vendor/bin/sail npm run build
 ./vendor/bin/sail php artisan key:generate
 ./vendor/bin/sail php artisan migrate --seed
 
